24ac20365e1306da0f76bdc36844c679ac3eba5f
dhubleur <dhubleur@student.42.fr>
2022-11-10 13:28:57 +0100

    test

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 20:40:50 by jmaia             #+#    #+#             */
-/*   Updated: 2022/11/10 13:06:34 by dhubleur         ###   ########.fr       */
+/*   Updated: 2022/11/10 13:28:45 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,12 @@
 #include <vector>
 #include <errno.h>
 #include <string.h>
+#include <poll.h>
 
 #include "parser.hpp"
+
+#define PORT 3333
+#define POLL_SIZE 1024
 
 int	main(int ac, char **av)
 {
@@ -33,10 +37,11 @@ int	main(int ac, char **av)
 	(void)ac;
 	(void)av;
 
-	const int PORT = 3333;
+	struct pollfd pollSet[POLL_SIZE];
+	int	inPoll = 0;
 
-	int socketFd = socket(AF_INET, SOCK_STREAM, 0);
-	if (socketFd == -1)
+	int serverSocketFd = socket(AF_INET, SOCK_STREAM, 0);
+	if (serverSocketFd == -1)
 	{
 		std::cout << "Error: socket creation failed: " << strerror(errno) << std::endl;
 		return (1);
@@ -47,44 +52,63 @@ int	main(int ac, char **av)
     serverAddress.sin_family = AF_INET;  
     serverAddress.sin_addr.s_addr = INADDR_ANY;  
     serverAddress.sin_port = htons(PORT);
-	if (bind(socketFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+	if (bind(serverSocketFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
 	{
 		std::cout << "Error: bind failed: " << strerror(errno) << std::endl;
 		return (1);
 	}
 	std::cout << "Bind done on port " << PORT << std::endl;
 
-	const int maxPendingConnections = 10;
-	if (listen(socketFd, maxPendingConnections) < 0)
+	pollSet[0].fd = serverSocketFd;
+    pollSet[0].events = POLLIN;
+    inPoll++;
+
+	while (1)
 	{
-		std::cout << "Error: listen failed: " << strerror(errno) << std::endl;
-		return (1);
+		std::cout << "Enter in poll" << std::endl;
+		poll(pollSet, inPoll, 1000);
+		for(int i = 0; i < inPoll; i++)
+		{
+			if(pollSet[i].events == POLLIN)
+			{
+				if(pollSet[i].fd == serverSocketFd)
+				{
+					//Read on server socket => new connection
+					std::cout << "New connection" << std::endl;
+					struct sockaddr_in clientAddress;
+			       	socklen_t clientAddressLength = sizeof(clientAddress);
+			      	int clientserverSocketFd = accept(serverSocketFd, (struct sockaddr *)&clientAddress, &clientAddressLength);
+					if (clientserverSocketFd == -1)
+					{
+						std::cout << "Error: accept failed: " << strerror(errno) << std::endl;
+						return (1);
+					}
+					pollSet[inPoll].fd = clientserverSocketFd;
+					pollSet[inPoll].events = POLLIN;
+					inPoll++;
+				}
+				else
+				{
+					//Read on client socket => new message
+					std::cout << "New message" << std::endl;
+					char buffer[1024];
+					int n = read(pollSet[i].fd, buffer, 1024);
+					if (n == -1)
+					{
+						std::cout << "Error: read failed: " << strerror(errno) << std::endl;
+						return (1);
+					}
+					else
+					{
+						std::cout << buffer << std::endl;
+						if(strcmp(buffer, "exit") == 0)
+						{
+							close(serverSocketFd);
+							return (0);
+						}
+					}
+				}
+			}
+		}
 	}
-	std::cout << "Listening started... " << std::endl;
-
-
-	struct sockaddr_in clientAddress;
-	socklen_t clientAddressLength = sizeof(clientAddress);
-	int clientSocketFd = accept(socketFd, (struct sockaddr *)&clientAddress, &clientAddressLength);
-	if (clientSocketFd < 0)
-	{
-		std::cout << "Error: accept failed: " << strerror(errno) << std::endl;
-		return (1);
-	}
-	std::cout << "Connection accepted from " << inet_ntoa(clientAddress.sin_addr) << std::endl;
-
-	char buffer[1025];
-	bzero(buffer, 1025);
-	int readBytes = recv(clientSocketFd, buffer, 1024, 0);
-	if (readBytes < 0)
-	{
-		std::cout << "Error: read failed: " << strerror(errno) << std::endl;
-		return (1);
-	}
-	std::cout << "Message received: " << buffer << std::endl;
-	send(clientSocketFd, buffer, readBytes, 0);
-	
-
-	close(clientSocketFd);
-    close(socketFd);
 }
