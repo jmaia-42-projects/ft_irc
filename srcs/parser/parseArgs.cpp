9f5d9b39f2d18ec5769075808662ac6ee9a68f88
Jeremy Maia <jmaia@student.42.fr>
2022-11-11 18:20:29 +0100

    Add parseArgs()

--- srcs/parser/parseArgs.cpp
+++ srcs/parser/parseArgs.cpp
@@ -1,21 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.hpp                                         :+:      :+:    :+:   */
+/*   parseArgs.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/08 22:06:14 by jmaia             #+#    #+#             */
-/*   Updated: 2022/11/11 18:18:13 by jmaia            ###   ########.fr       */
+/*   Created: 2022/11/11 18:08:04 by jmaia             #+#    #+#             */
+/*   Updated: 2022/11/11 18:18:19 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSER_HPP
-# define PARSER_HPP
+#include "parser.hpp"
+#include "convert.hpp"
 
-#include <string>
+int	parseArgs(int ac, char **av, unsigned short &port, std::string &password)
+{
+	int	err;
 
-int	checkArgsValidity(int ac, char **av);
-int	parseArgs(int ac, char **av, unsigned short &port, std::string &password);
-
-#endif
+	err = checkArgsValidity(ac, av);
+	if (err)
+		return (err);
+	port = (unsigned short) toInt(std::string(av[1]));
+	password = std::string(av[2]);
+	return (0);
+}
