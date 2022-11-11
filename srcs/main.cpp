aefc26a22fda1dceec3463bb4252f91d9834af88
Jeremy Maia <jmaia@student.42.fr>
2022-11-11 18:28:36 +0100

    Init listening socket

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,22 +6,27 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 20:40:50 by jmaia             #+#    #+#             */
-/*   Updated: 2022/11/11 18:21:38 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/11/11 18:27:50 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sstream>
 #include <string>
 
+#include "initSocket.hpp"
 #include "parser.hpp"
 
 int	main(int ac, char **av)
 {
 	int				err;
+	int				listen_fd;
 	unsigned short	port;
 	std::string		password;
 
 	err = parseArgs(ac, av, port, password);
 	if (err)
-		return (err);
+		return (1);
+	listen_fd = initSocketOrPrintError(port);
+	if (!listen_fd)
+		return (2);
 }
