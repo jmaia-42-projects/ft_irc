d69861935064f20f8be8b7d039b295753d80e4e7
dhubleur <dhubleur@student.42.fr>
2022-11-13 12:54:23 +0100

    separe in functions

--- srcs/poll.cpp
+++ srcs/poll.cpp
@@ -6,13 +6,13 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 17:19:38 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/11/13 12:51:02 by dhubleur         ###   ########.fr       */
+/*   Updated: 2022/11/13 12:53:51 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "poll.hpp"
 
-void    exportPollSet(struct pollfd *pollSet, int serverSocket, std::vector<Client> &clients)
+void exportPollSet(struct pollfd *pollSet, int serverSocket, std::vector<Client> &clients)
 {
     pollSet[0].fd = serverSocket;
     pollSet[0].events = POLLIN;
@@ -20,29 +20,29 @@ void    exportPollSet(struct pollfd *pollSet, int serverSocket, std::vector<Clie
         pollSet[i + 1] = clients.at(i).getPollFd();
 }
 
-void    acceptConnection(int serverSocket, std::vector<Client> &clients)
+void acceptConnection(int serverSocket, std::vector<Client> &clients)
 {
     struct sockaddr_in clientAddress;
     socklen_t clientAddressLength = sizeof(clientAddress);
-	int clientserverSocketFd = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
-	if (clientserverSocketFd == -1)
-	{
-		std::cout << "Error: accept failed: " << strerror(errno) << std::endl;
-		return;
-	}
+    int clientserverSocketFd = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
+    if (clientserverSocketFd == -1)
+    {
+        std::cout << "Error: accept failed: " << strerror(errno) << std::endl;
+        return;
+    }
     Client client(clientserverSocketFd);
     std::cout << "New connection: Client " << client.getId() << std::endl;
     clients.push_back(client);
 }
 
-std::string    acceptMessage(Client &client)
+std::string acceptMessage(Client &client)
 {
     std::string message;
-    char        buffer[REVC_BUFFER];
-    int         readed;
+    char buffer[REVC_BUFFER];
+    int readed;
 
     bzero(buffer, REVC_BUFFER);
-    while((readed = recv(client.getSocket(), buffer, REVC_BUFFER, MSG_DONTWAIT)) > 0)
+    while ((readed = recv(client.getSocket(), buffer, REVC_BUFFER, MSG_DONTWAIT)) > 0)
     {
         if (readed == -1)
         {
@@ -52,14 +52,45 @@ std::string    acceptMessage(Client &client)
         message.append(buffer, readed);
         bzero(buffer, REVC_BUFFER);
     }
-    return message;  
+    return message;
 }
 
-void    pollRoutine(int serverSocket)
+void treatPollFd(struct pollfd &pollSet, int serverSocket, std::vector<Client> &clients)
+{
+    if (pollSet.revents & POLLIN)
+    {
+        if (pollSet.fd == serverSocket)
+            acceptConnection(serverSocket, clients);
+        else
+        {
+            for (size_t j = 0; j < clients.size(); j++)
+            {
+                if (clients.at(j).getSocket() == pollSet.fd)
+                {
+                    Client &client = clients.at(j);
+                    std::string message = acceptMessage(client);
+                    if (message.length() > 0)
+                    {
+                        std::cout << "------ New message received from client " << client.getId() << " ------" << std::endl
+                                  << message << std::endl;
+                        treatMessage(message, client, clients);
+                    }
+                    else
+                    {
+                        std::cout << "Client " << client.getId() << " disconnected" << std::endl;
+                        clients.erase(clients.begin() + j);
+                    }
+                }
+            }
+        }
+    }
+}
+
+void pollRoutine(int serverSocket)
 {
     std::vector<Client> clients;
-    size_t              inPoll;
-    
+    size_t inPoll;
+
     while (1)
     {
         inPoll = clients.size() + 1;
@@ -67,33 +98,6 @@ void    pollRoutine(int serverSocket)
         exportPollSet(pollSet, serverSocket, clients);
         poll(pollSet, inPoll, -1);
         for (size_t i = 0; i < inPoll; i++)
-        {
-            if (pollSet[i].revents & POLLIN)
-            {
-                if (pollSet[i].fd == serverSocket)
-                    acceptConnection(serverSocket, clients);
-                else
-                {
-                    for(size_t j = 0; j < clients.size(); j++)
-                    {
-                        if (clients.at(j).getSocket() == pollSet[i].fd)
-                        {
-                            Client &client = clients.at(j);
-                            std::string message = acceptMessage(client);
-                            if (message.length() > 0)
-                            {
-                                std::cout << "------ New message received from client " << client.getId() << " ------" << std::endl << message << std::endl;
-                                treatMessage(message, client, clients);
-                            }
-                            else
-                            {
-                                std::cout << "Client " << client.getId() << " disconnected" << std::endl;
-                                clients.erase(clients.begin() + j);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+            treatPollFd(pollSet[i], serverSocket, clients);
     }
 }
